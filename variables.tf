61b13838eb080a26e1b88f066a2e6b763cf64e67
Lee <lamoss1985@gmail.com>
2024-12-24 16:52:00 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -2,16 +2,22 @@ variable "admin_password" {
   type = string
 }
 
-variable "admin_username" {
-  type = string
-}
-
 variable "key_vault_name" {
   type = string
 }
 
 variable "key_vault_rg" {
   type = string
+}
+
+variable "admin_username_secret" {
+  type = string
+  default = "vm-admin-username"
+}
+
+variable "admin_password_secret" {
+  type = string
+  default = "vm-password"
 }
 
 variable "admin_username" {
