91c1b5c0be3f05320d277e5806fae06bfe6849ca
Lee Moss <lee.moss@dvla.gov.uk>
2025-01-15 16:47:20 +0000

    init

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "NetflixProject"
-    storage_account_name = "tfstatest"
-    container_name       = "tfstate"
-    key                  = "netflix.tfstate"
-  }
-} 
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-admin_password = "Flowlogs2026?!?"
-key_vault_name = "NetflixSecret"
-key_vault_rg   = "NetflixProject" 
