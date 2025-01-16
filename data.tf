e194daea155bb24f55e20bd2327a939e6b8e1e4c
Lee Moss <lee.moss@dvla.gov.uk>
2025-01-16 08:39:16 +0000

    kv

--- data.tf
+++ data.tf
@@ -1,4 +1,1 @@
-data "azurerm_key_vault" "netflix" {
-  name                = var.key_vault_name
-  resource_group_name = var.key_vault_rg
-}
+data "azurerm_client_config" "current" {}
--- key-vault.tf
+++ key-vault.tf
@@ -1,16 +1,16 @@
 resource azurerm_key_vault_secret NetflixSecret {
   name         = "NetflixSecret"
   value        = "your-secret-value"
-  key_vault_id = data.azurerm_key_vault.netflix.id
+  key_vault_id = azurerm_key_vault.netflix.id
 }
 
-resource "random_string" "random" {
+resource random_string random {
   length  = 8
   special = false
   upper   = false
 }
 
-resource "azurerm_key_vault" "netflix" {
+resource azurerm_key_vault netflix {
   name                = "netflix-kv-${random_string.random.result}"
   location            = azurerm_resource_group.netflix.location
   resource_group_name = azurerm_resource_group.netflix.name
@@ -27,13 +27,13 @@ resource "azurerm_key_vault" "netflix" {
   }
 }
 
-resource "azurerm_key_vault_secret" "admin_username" {
+resource azurerm_key_vault_secret admin_username {
   name         = "vm-admin-username"
   value        = var.admin_username
   key_vault_id = azurerm_key_vault.netflix.id
 }
 
-resource "azurerm_key_vault_secret" "admin_password" {
+resource azurerm_key_vault_secret admin_password {
   name         = "vm-password"
   value        = var.admin_password
   key_vault_id = azurerm_key_vault.netflix.id
