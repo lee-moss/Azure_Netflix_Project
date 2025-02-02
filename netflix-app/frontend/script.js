0d0f4e456c1991760cde750092b263d73ee23d70
Lee Moss <lee.moss@dvla.gov.uk>
2025-02-02 16:01:03 +0000

    security: Move API key to environment variables

--- netflix-app/frontend/script.js
+++ netflix-app/frontend/script.js
@@ -1,10 +1,13 @@
-const API_KEY = '36ea7942e80240d2463ca4826e1a8527';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+// Config object to store API configuration
+const config = {
+    apiKey: '${TMDB_API_KEY}', // This will be replaced by the build process
+    baseUrl: 'https://api.themoviedb.org/3',
+    imageBaseUrl: 'https://image.tmdb.org/t/p/w500'
+};
 
 async function fetchTrendingMovies() {
     try {
-        const response = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
+        const response = await fetch(`${config.baseUrl}/trending/movie/week?api_key=${config.apiKey}`);
         const data = await response.json();
         return data.results;
     } catch (error) {
@@ -18,7 +21,7 @@ function createMovieCard(movie) {
     movieCard.className = 'movie-card';
     
     const image = document.createElement('img');
-    image.src = `${IMAGE_BASE_URL}${movie.poster_path}`;
+    image.src = `${config.imageBaseUrl}${movie.poster_path}`;
     image.alt = movie.title;
     
     const title = document.createElement('div');
