82dad8c33a95efee48ea2a6893622c56050b0f5b
Lee Moss <lee.moss@dvla.gov.uk>
2025-02-08 17:42:05 +0000

    deploy fix

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,3 +1,9 @@
+resource random_string random {
+  length  = 8
+  special = false
+  upper   = false
+}
+
 resource azurerm_key_vault vault {
   name                         = "${var.key_vault_name}-${random_string.random.result}"
   location                     = var.location
