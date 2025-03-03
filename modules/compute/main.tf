d30628c6f9ba613ced61d730110ce36c8838f4e7
Lee Moss <lee.moss@dvla.gov.uk>
2025-03-03 18:29:21 +0000

    was missing a pip

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,4 +1,13 @@
 # Network Interface
+resource azurerm_public_ip vm_public_ip {
+  name                = "vm-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
 resource azurerm_network_interface nic {
   name                = "vm-nic"
   location            = var.location
@@ -8,6 +17,7 @@ resource azurerm_network_interface nic {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id         = azurerm_public_ip.vm_public_ip.id
   }
 
   tags = var.tags
--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -17,4 +17,14 @@ output admin_username {
 output ssh_public_key {
   description = "Public key for SSH access"
   value       = tls_private_key.ssh_key.public_key_openssh
+}
+
+output vm_public_ip {
+  description = "The public IP address of the virtual machine"
+  value       = azurerm_public_ip.vm_public_ip.ip_address
+}
+
+output vm_public_ip_fqdn {
+  description = "The FQDN of the virtual machine's public IP address"
+  value       = azurerm_public_ip.vm_public_ip.fqdn
 } 
